0b7d5db2ddb8bf7c9c1c9f6dca9b22b7e071f791
vcpersike <vcpersike@gmail.com>
2024-12-23 20:21:42 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'minha-pagina',
+  webDir: 'www'
+};
+
+export default config;
