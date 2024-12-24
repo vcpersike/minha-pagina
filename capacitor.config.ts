4c1bb88504515fe6abaaec61d78ef21eff0fac9c
vcpersike <vcpersike@gmail.com>
2024-12-24 03:23:16 -0300

    incluindo build para android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'minha-pagina',
-  webDir: 'www'
+  webDir: 'www/browser',
+  bundledWebRuntime: false,
 };
 
 export default config;
